e03db585478b07db8a5902304c5110f442e0d9a2
r0bin <r0bin_dood@pm.me>
2024-11-11 16:11:00 -0500

    slab allocator improvements and thread safety

--- src/alloc/lcu_slab_alloc.c
+++ src/alloc/lcu_slab_alloc.c
@@ -3,12 +3,11 @@
 #include "lcu_alloc.h"
 #include "lcu_slab_alloc.h"
 
-#define ALLOC_MEMORY_MARK 0x11
+#define ALLOC_MEMORY_MARK 0xDE
 
-#define ADJUST_CHUNK_ADDRESS(chunk, add_offset)  \
-    (void *)((uint8_t *)(chunk) + ((add_offset) ? (sizeof(void *) + 1) : -(sizeof(void *) + 1)))
-
-#define DEREF_CHUNK_MARK(chunk)  (*((uint8_t *)(chunk) + sizeof(void *)))
+#define CHUNK_DATA(chunk)       (void *)((uint8_t *)(chunk) + (sizeof(void *) + 1))
+#define CHUNK_METADATA(chunk)   (void *)((uint8_t *)(chunk) - (sizeof(void *) + 1))
+#define DEREF_CHUNK_MARK(chunk) (*((uint8_t *)(chunk) + sizeof(void *)))
 
 typedef struct slab {
     uint8_t *chunk;
@@ -16,6 +15,7 @@ typedef struct slab {
 } slab_t;
 
 typedef struct slab_alloc {
+    atomic_flag flag;
     slab_t *slab;
     slab_t *slab_tail;
     void *free_list;
@@ -30,12 +30,12 @@ static void insert_free_list(slab_alloc_t *sa, slab_t *slab);
 static void push_free_list(slab_alloc_t *sa, void *chunk);
 static void *pop_free_list(slab_alloc_t *sa);
 static bool is_in_free_list(slab_alloc_t *sa, void *addr);
-static bool is_valid_chunk_address(slab_alloc_t *sa, void *addr);
 
 lcu_slab_alloc_t lcu_slab_alloc_create(size_t chunk_size, size_t chunks_per_slab)
 {
     slab_alloc_t *sa = lcu_zalloc(sizeof(slab_alloc_t));
     if (sa != NULL) {
+        atomic_flag_clear(&sa->flag);
         sa->chunks_per_slab = chunks_per_slab;
         sa->chunk_size = chunk_size;
         sa->chunk_size_total = chunk_size * chunks_per_slab;
@@ -55,10 +55,15 @@ void *lcu_slab_alloc(lcu_slab_alloc_t handle)
     
     slab_alloc_t *sa = (slab_alloc_t *)handle;
 
-    if (sa->free_list == NULL) {
-        if (insert_slab(sa) == NULL)
-            return NULL;
-    }
+    bool ret_null = false;
+    ATOMIC_SPINLOCK(sa->flag, {
+        if (sa->free_list == NULL) {
+            if (insert_slab(sa) == NULL)
+                ret_null = true;
+        }
+    });
+    if (ret_null == true)
+        return NULL;
     
     return pop_free_list(sa);
 }
@@ -70,11 +75,11 @@ void lcu_slab_free(lcu_slab_alloc_t handle, void *addr)
 
     slab_alloc_t *sa = (slab_alloc_t *)handle;
 
-    if (is_in_free_list(sa, addr) == true)
-        return;
-
-    if (is_valid_chunk_address(sa, addr) == true)
-        push_free_list(sa, addr);
+    if (is_in_free_list(sa, addr) == false) {
+        ATOMIC_SPINLOCK(sa->flag, {
+            push_free_list(sa, addr);
+        });
+    }
 }
 
 void lcu_slab_alloc_destroy(lcu_slab_alloc_t *handle)
@@ -84,19 +89,20 @@ void lcu_slab_alloc_destroy(lcu_slab_alloc_t *handle)
 
     slab_alloc_t *sa = (slab_alloc_t *)(*handle);
 
-    slab_t *slab = sa->slab;
-    slab_t *next_slab;
-    while (slab != NULL) {
-        next_slab = slab->next;
-        lcu_free(slab->chunk);
-        lcu_free(slab);
-        slab = next_slab;
-    }
-    sa->slab = NULL;
-    sa->slab_tail = NULL;
-
-    lcu_free(sa);
-    *handle = NULL;
+    ATOMIC_SPINLOCK(sa->flag, {
+        slab_t *slab = sa->slab;
+        slab_t *next_slab;
+        while (slab != NULL) {
+            next_slab = slab->next;
+            lcu_free(slab->chunk);
+            lcu_free(slab);
+            slab = next_slab;
+        }
+        sa->slab = NULL;
+        sa->slab_tail = NULL;
+        lcu_free(sa);
+        *handle = NULL;
+    });
 }
 
 slab_t *insert_slab(slab_alloc_t *sa)
@@ -107,14 +113,12 @@ slab_t *insert_slab(slab_alloc_t *sa)
             sa->slab_tail->next = slab;
         sa->slab_tail = slab;
         slab->next = NULL;
-
         size_t total_chunk_size_with_free_list = sa->chunk_size_total + sa->chunks_per_slab * (sizeof(void *) + 1);
         slab->chunk = lcu_zalloc(total_chunk_size_with_free_list);
         if (slab->chunk == NULL) {
             lcu_free(slab);
             return NULL;
         }
-
         insert_free_list(sa, slab);
     }
     return slab;
@@ -125,13 +129,13 @@ void insert_free_list(slab_alloc_t *sa, slab_t *slab)
     void *chunk;
     for (size_t i = 0; i < sa->chunks_per_slab; i++) {
         chunk = (void *)(slab->chunk + (i * sa->chunk_size));
-        push_free_list(sa, ADJUST_CHUNK_ADDRESS(chunk, true));
+        push_free_list(sa, CHUNK_DATA(chunk));
     }
 }
 
 void push_free_list(slab_alloc_t *sa, void *chunk)
 {
-    chunk = ADJUST_CHUNK_ADDRESS(chunk, false);
+    chunk = CHUNK_METADATA(chunk);
     DEREF_CHUNK_MARK(chunk) = 0;
     *(void **)chunk = NULL;
     if (sa->free_list == NULL) {
@@ -145,34 +149,23 @@ void push_free_list(slab_alloc_t *sa, void *chunk)
 
 void *pop_free_list(slab_alloc_t *sa)
 {
-    if (sa->free_list == NULL)
+    void *chunk = NULL;
+    ATOMIC_SPINLOCK(sa->flag, {
+        if (sa->free_list != NULL) {
+            chunk = sa->free_list;
+            sa->free_list = *(void **)chunk;
+        }
+    });
+    if (chunk == NULL)
         return NULL;
-    void *chunk = sa->free_list;
-    sa->free_list = *(void **)chunk;
     DEREF_CHUNK_MARK(chunk) = ALLOC_MEMORY_MARK;
-    return ADJUST_CHUNK_ADDRESS(chunk, true);
+    return CHUNK_DATA(chunk);
 }
 
 bool is_in_free_list(slab_alloc_t *sa, void *addr)
 {
-    void *chunk = ADJUST_CHUNK_ADDRESS(addr, false);
+    void *chunk = CHUNK_METADATA(addr);
     if (DEREF_CHUNK_MARK(chunk) == ALLOC_MEMORY_MARK)
         return false;
     return true;
-}
-
-bool is_valid_chunk_address(slab_alloc_t *sa, void *addr)
-{
-    slab_t *slab = sa->slab;
-    while (slab != NULL) {
-        uint8_t *slab_start = slab->chunk;
-        uint8_t *slab_end = slab_start + sa->chunk_size_total;
-        if (addr >= (void *)slab_start && addr < (void *)slab_end) {
-            size_t offset = (uintptr_t)addr - (uintptr_t)slab_start;
-            if (offset % sa->chunk_size == 0)
-                return true;
-        }
-        slab = slab->next;
-    }
-    return false;
 }
